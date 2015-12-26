951a01939d8ec2b8444dc292cfb436ebddff9586
budblack <budblack@qq.com>
2015-12-26 17:11:09 +0800

    使用七牛CND分发

--- Extensions/Chrome/IngressIntel2GCJ/Init.js
+++ Extensions/Chrome/IngressIntel2GCJ/Init.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     console.log("Page loaded.");
     $('script[src*="//maps.googleapis.com/maps/api/js?client=gme-nianticinc&sensor=true&v=3.12"]').remove();
-    $("body").append('<script src="https://budblack.me/Lab/Ingress/IngressIntel2GCJ/JSHacker/Ingress.js" type="text/javascript" />');
-    console.assert("Done.");
+    $("body").append('<script src="https://dn-budblack.qbox.me/Lab/Ingress/IngressIntel2GCJ/JSHacker/Ingress.js" type="text/javascript" />');
+    console.assert("Script Done.");
 });

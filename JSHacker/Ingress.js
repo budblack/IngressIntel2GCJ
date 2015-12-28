14f7199490c4bb2cffb7f9a5e467d780705f59c8
budblack <budblack@qq.com>
2015-12-28 17:55:43 +0800

    bibibibi

--- JSHacker/Ingress.js
+++ JSHacker/Ingress.js
@@ -1,3 +1,4 @@
+console.log("IngressIntel2GCJ loading.");
 //Marker
 function m_ii(a, b) {
     //console.log("m_ii");
@@ -73,7 +74,6 @@ function m_ii(a, b) {
     }
     ki(a)
 }
-
 //Polygon
 function m_Ff(a) {
     //console.log("m_Ff");
@@ -88,7 +88,6 @@ function m_Ff(a) {
     //console.log("=========");
     return null != a.latLng ? Cf(a.latLng) : null
 }
-
 //Polyline
 function m_cj(a, b, c, d, f, g) {
     this.f = new Ef(a, b, c);
@@ -154,21 +153,21 @@ function Jj(a) {
     wh(c, f, p);
     return b
 }
-
-console.log("My Ingress loading.");
+console.log("Hacking function.");
+m_ii.prototype = ii.prototype;
 ii = m_ii;
+m_Ff.prototype = Ff.prototype;
 Ff = m_Ff;
 m_cj.prototype = cj.prototype;
 cj = m_cj;
-console.log("Hacking function");
-//Ext
+console.log("Good boy.")
+//Ext, required from https://github.com/googollee/eviltransform
 var exports
 if (typeof module === "object" && exports) {
     exports = module.exports
 } else if (typeof window !== "undefined") {
     exports = window["eviltransform"] = {}
 }
-
 function outOfChina(lat, lng) {
     if ((lng < 72.004) || (lng > 137.8347)) {
         return true;
@@ -178,7 +177,6 @@ function outOfChina(lat, lng) {
     }
     return false;
 }
-
 function transformLat(x, y) {
     var ret = -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*x*y + 0.2*Math.sqrt(Math.abs(x));
     ret += (20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI)) * 2.0 / 3.0;
@@ -186,7 +184,6 @@ function transformLat(x, y) {
     ret += (160.0*Math.sin(y/12.0*Math.PI) + 320*Math.sin(y*Math.PI/30.0)) * 2.0 / 3.0;
     return ret;
 }
-
 function transformLon(x, y) {
     var ret = 300.0 + x + 2.0*y + 0.1*x*x + 0.1*x*y + 0.1*Math.sqrt(Math.abs(x));
     ret += (20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI)) * 2.0 / 3.0;
@@ -194,7 +191,6 @@ function transformLon(x, y) {
     ret += (150.0*Math.sin(x/12.0*Math.PI) + 300.0*Math.sin(x/30.0*Math.PI)) * 2.0 / 3.0;
     return ret;
 }
-
 function delta(lat, lng) {
     var a = 6378245.0;
     var ee = 0.00669342162296594323;
@@ -208,7 +204,6 @@ function delta(lat, lng) {
     dLng = (dLng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * Math.PI);
     return {"lat": dLat, "lng": dLng};
 }
-
 function wgs2gcj(wgsLat, wgsLng) {
     if (outOfChina(wgsLat, wgsLng)) {
         return {"lat": wgsLat, "lng": wgsLng};
@@ -217,102 +212,4 @@ function wgs2gcj(wgsLat, wgsLng) {
     return {"lat": wgsLat + d.lat, "lng": wgsLng + d.lng};
 }
 exports.wgs2gcj = wgs2gcj;
-
-function gcj2wgs(gcjLat, gcjLng) {
-    if (outOfChina(gcjLat, gcjLng)) {
-        return {"lat": gcjLat, "lng": gcjLng};
-    }
-    var d = delta(gcjLat, gcjLng);
-    return {"lat": gcjLat - d.lat, "lng": gcjLng - d.lng};
-}
-exports.gcj2wgs = gcj2wgs;
-
-function gcj2wgs_exact(gcjLat, gcjLng) {
-    var initDelta = 0.01;
-    var threshold = 0.000001;
-    var dLat = initDelta, dLng = initDelta;
-    var mLat = gcjLat-dLat, mLng = gcjLng-dLng;
-    var pLat = gcjLat+dLat, pLng = gcjLng+dLng;
-    var wgsLat, wgsLng;
-    for (var i = 0; i < 30; i++) {
-        wgsLat = (mLat+pLat)/2;
-        wgsLng = (mLng+pLng)/2;
-        var tmp = wgs2gcj(wgsLat, wgsLng)
-        dLat = tmp.lat-gcjLat;
-        dLng = tmp.lng-gcjLng;
-        if ((Math.abs(dLat) < threshold) && (Math.abs(dLng) < threshold)) {
-            return {"lat": wgsLat, "lng": wgsLng};
-        }
-        if (dLat > 0) {
-            pLat = wgsLat;
-        } else {
-            mLat = wgsLat;
-        }
-        if (dLng > 0) {
-            pLng = wgsLng;
-        } else {
-            mLng = wgsLng;
-        }
-    }
-    return {"lat": wgsLat, "lng": wgsLng};
-}
-exports.gcj2wgs_exact = gcj2wgs_exact;
-
-function distance(latA, lngA, latB, lngB) {
-    var earthR = 6371000;
-    var x = Math.cos(latA*Math.PI/180) * Math.cos(latB*Math.PI/180) * Math.cos((lngA-lngB)*Math.PI/180);
-    var y = Math.sin(latA*Math.PI/180) * Math.sin(latB*Math.PI/180);
-    var s = x + y;
-    if (s > 1) {
-        s = 1;
-    }
-    if (s < -1) {
-        s = -1;
-    }
-    var alpha = Math.acos(s);
-    var distance = alpha * earthR;
-    return distance;
-}
-exports.distance = distance;
-
-function gcj2bd(gcjLat, gcjLng) {
-    if (outOfChina(gcjLat, gcjLng)) {
-        return {"lat": gcjLat, "lng": gcjLng};
-    }
-
-    var x = gcjLng, y = gcjLat;
-    var z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * Math.PI);
-    var theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * Math.PI);
-    var bdLng = z * Math.cos(theta) + 0.0065;
-    var bdLat = z * Math.sin(theta) + 0.006;
-    return {"lat": bdLat, "lng": bdLng};
-}
-exports.gcj2bd = gcj2bd;
-
-function bd2gcj(bdLat, bdLng) {
-    if (outOfChina(bdLat, bdLng)) {
-        return {"lat": bdLat, "lng": bdLng};
-    }
-
-    var x = bdLng - 0.0065, y = bdLat - 0.006;
-    var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * Math.PI);
-    var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * Math.PI);
-    var gcjLng = z * Math.cos(theta);
-    var gcjLat = z * Math.sin(theta);
-    return {"lat": gcjLat, "lng": gcjLng};
-}
-exports.bd2gcj = bd2gcj;
-
-function wgs2bd(wgsLat, wgsLng) {
-    var gcj = wgs2gcj(wgsLat, wgsLng)
-    return gcj2bd(gcj.lat, gcj.lng)
-}
-exports.wgs2bd = wgs2bd;
-
-function bd2wgs(bdLat, bdLng) {
-    var gcj = bd2gcj(bdLat, bdLng)
-    return gcj2wgs(gcj.lat, gcj.lng)
-}
-exports.bd2wgs = bd2wgs;
-
-console.log("My Ingress loaded.");
+console.log("IngressIntel2GCJ loaded.");

954c7198838cb25616c5ba3a64570bbd82643ae4
budblack <budblack@qq.com>
2015-12-30 15:43:19 +0800

    写方法读取当前玩家的资料，替换模板中的__var_PLAYER_INF__

--- Extensions/Chrome/IngressIntel2GCJ/js/Init.js
+++ Extensions/Chrome/IngressIntel2GCJ/js/Init.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-    $.get(chrome.getURL(""))
+$.get(chrome.extension.getURL("teamplate/Ingress.html"),function(IngressHtml){
+    $.get("https://www.ingress.com/intel",function(BaseHtml){
+        var Player = BaseHtml.match(/var PLAYER = \{.+\};/g);
+        //console.log(Player);
+        IngressHtml = IngressHtml.replace("__var_PLAYER_INF__",Player);
+        document.open();
+        document.write(IngressHtml);
+        document.close();
+    });
 });

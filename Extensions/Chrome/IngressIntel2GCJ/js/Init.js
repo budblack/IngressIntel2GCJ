fb256721c16957c7071c0986a56dcdf0feac5131
budblack <budblack@qq.com>
2015-12-30 14:11:44 +0800

    写模板

--- Extensions/Chrome/IngressIntel2GCJ/js/Init.js
+++ Extensions/Chrome/IngressIntel2GCJ/js/Init.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-    $('script[src*="//maps.googleapis.com/maps/api/js?client=gme-nianticinc&sensor=true&v=3.12"]').remove();
-    $("body").append('<script src="https://budblack.me/Lab/Ingress/IngressIntel2GCJ/JSHacker/Ingress.js" type="text/javascript" />');
-    //Baidu tongji
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "//hm.baidu.com/hm.js?838220e6c6be41482d150e5b1c46257d";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
+    $.get(chrome.getURL(""))
 });

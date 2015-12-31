3bd2703c3c3d64e0094ce57a06dc79b8d1491fd3
budblack <budblack@qq.com>
2015-12-31 17:35:15 +0800

    make a template

--- Extensions/Chrome/IngressIntel2GCJ/js/Init.js
+++ Extensions/Chrome/IngressIntel2GCJ/js/Init.js
@@ -1,10 +1,14 @@
 $.get(chrome.extension.getURL("teamplate/Ingress.html"),function(IngressHtml){
     $.get("https://www.ingress.com/intel",function(BaseHtml){
         var Player = BaseHtml.match(/var PLAYER = \{.+\};/g);
+        var HeaderLoginInfo = BaseHtml.match(/<span class="pointer" id="header_email">.+@.+.com<\/span>&nbsp;/g);
         //console.log(Player);
-        IngressHtml = IngressHtml.replace("__var_PLAYER_INF__",Player);
-        document.open();
-        document.write(IngressHtml);
-        document.close();
+        if (Player) {
+            IngressHtml = IngressHtml.replace("__var_PLAYER_INF__", Player);
+            IngressHtml = IngressHtml.replace("__Header_Login_Info__",HeaderLoginInfo);
+            document.open();
+            document.write(IngressHtml);
+            document.close();
+        }
     });
 });
